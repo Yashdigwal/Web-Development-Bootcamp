21418d1c67cda19c8ada67f539edaf4b7eba2dc6
Yashdigwal <48600680+Yashdigwal@users.noreply.github.com>
2021-07-09 16:18:15 +0530

    Add files via upload

---
+++ function_Age_ Program.js
@@ -0,0 +1,16 @@
+//using Function with Parameter
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    var leftyear=90-age;
+    var days=leftyear*365;
+    var weeks=leftyears*52;
+    var months=leftyears*12;
+    
+    console.log("you have"+days+"days,"+weeks+"weeks, and"+ months+"months left");
+    
+/*************Don't change the code below**********/
+}
+lifeInWeeks(51)

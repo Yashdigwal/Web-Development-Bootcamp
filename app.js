66033cb3f2561c846aabe519b5ae54122e18db34
Yashdigwal <yashdigwal66@gmail.com>
2021-07-13 21:58:42 +0530

    blog Website

---
+++ app.js
@@ -0,0 +1,70 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const _ = require("lodash");
+
+const homeStartingContent ="Our mission is to organise the world’s information and make it universally accessible and useful.";
+const aboutContent = "Google LLC is an American multinational technology company that specializes in Internet-related services and products, which include online advertising technologies, a search engine, cloud computing, software, and hardware. Wikipedia";
+const contactContent = "If your concern/ query is related to Google Pay, you can reach out to our product specific escalation teams via the below channels:- Google Pay India Grievance Center is available here.- You can also find details about how to contact Google Pay India support at the Help Center page here.- Customer Care number toll free at: 1-800-419-0157.";
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+let posts = [];
+
+app.get("/", function(req, res){
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts
+    });
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
+});
+
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+
+});
+
+app.get("/posts/:postName", function(req, res){
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post){
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
+
+});
+
+app.listen(4000, function() {
+  console.log("Server started on port 4000");
+});
